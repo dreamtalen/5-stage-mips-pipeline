a24454688f277e04022f074884210b2e0d1f7685
dreamtalen <dreamtalen@hotmail.com>
2014-12-01 10:13:53 +0800

    Revert "top and framework"
    This reverts commit 01f7b164cdd4f313335e320f4145b06766e8ddd8.

--- wb.v
+++
@@ -1,9 +0,0 @@
-module WB(
-	input [31:0]MEM_WB_aluOut,
-	input [31:0]MEM_WB_data,
-	input MEM_WB_memToReg,
-	output [31:0]writeData);
-
-	assign writeData = MEM_WB_memToReg?MEM_WB_data:MEM_WB_aluOut;
-	
-endmodule
